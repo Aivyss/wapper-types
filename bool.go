1f971aeea57de298c13980d99c132724c2cb91c6
Aivyss <hklee6417@gmail.com>
2023-10-04 00:36:32 +0900

    ADD some methods

--- bool.go
+++ bool.go
@@ -11,3 +11,23 @@ func (b Bool) Raw() bool {
 func (b Bool) ToStr() String {
 	return String(fmt.Sprintf("%v", b.Raw()))
 }
+
+func (b Bool) ToInt() Int {
+	if b {
+		return 1
+	}
+
+	return 0
+}
+
+func (b Bool) And(c Bool) Bool {
+	return b && c
+}
+
+func (b Bool) Or(c Bool) Bool {
+	return b || c
+}
+
+func (b Bool) Not() Bool {
+	return !b
+}
