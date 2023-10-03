d6d061349cac65e65b1ff3ee4b3a6ed036fcef61
Aivyss <hklee6417@gmail.com>
2023-10-03 22:06:18 +0900

    ADD some methods

--- float.go
+++ float.go
@@ -1,5 +1,90 @@
 package wrapperTypes
 
+import (
+	"fmt"
+	"math"
+)
+
 type Float64 float64
 
 type Float32 float32
+
+func (f *Float64) Raw() float64 {
+	return float64(*f)
+}
+
+func (f *Float64) Floor(x Float64) Float64 {
+	return Float64(math.Floor(x.Raw()))
+}
+
+func (f *Float64) Ceil(x Float64) Float64 {
+	return Float64(math.Ceil(x.Raw()))
+}
+
+func (f *Float64) Round(x Float64) Float64 {
+	return Float64(math.Round(x.Raw()))
+}
+
+func (f *Float64) Pow10(n Int) Float64 {
+	return Float64(math.Pow10(n.Raw()))
+}
+
+func (f *Float64) Pow(n Float64) Float64 {
+	return Float64(math.Pow(f.Raw(), n.Raw()))
+}
+
+func (f *Float64) Abs() Float64 {
+	if *f < 0 {
+		return -*f
+	}
+
+	return *f
+}
+
+func (f *Float64) ToStr() String {
+	return String(fmt.Sprintf("%v", f.Raw()))
+}
+
+func (f *Float64) ToInt() Int {
+	return Int(*f)
+}
+
+func (f *Float32) Raw() float64 {
+	return float64(*f)
+}
+
+func (f *Float32) Floor(x Float32) Float32 {
+	return Float32(math.Floor(x.Raw()))
+}
+
+func (f *Float32) Ceil(x Float32) Float32 {
+	return Float32(math.Ceil(x.Raw()))
+}
+
+func (f *Float32) Round(x Float32) Float32 {
+	return Float32(math.Round(x.Raw()))
+}
+
+func (f *Float32) Pow10(n Int) Float32 {
+	return Float32(math.Pow10(n.Raw()))
+}
+
+func (f *Float32) Pow(n Float32) Float32 {
+	return Float32(math.Pow(f.Raw(), n.Raw()))
+}
+
+func (f *Float32) Abs() Float32 {
+	if *f < 0 {
+		return -*f
+	}
+
+	return *f
+}
+
+func (f *Float32) ToStr() String {
+	return String(fmt.Sprintf("%v", f.Raw()))
+}
+
+func (f *Float32) ToInt() Int {
+	return Int(*f)
+}
--- int.go
+++ int.go
@@ -1,6 +1,9 @@
 package wrapperTypes
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Int int
 type Int8 int8
@@ -22,6 +25,47 @@ func (i *Int) Abs() Int {
 	return Int(raw)
 }
 
+func (i *Int) Pow(num Int) Int {
+	if num == 0 {
+		return 1
+	}
+
+	if num < 0 {
+		panic(errors.New("num < 0"))
+	}
+
+	result := *i
+	for j := 0; j < num.Raw()-1; j++ {
+		result *= *i
+	}
+
+	return result
+}
+
 func (i *Int) ToStr() String {
 	return String(fmt.Sprintf("%v", i.Raw()))
+}
+
+func (i *Int) ToInt64() Int64 {
+	return Int64(i.Raw())
+}
+
+func (i *Int) ToInt32() Int32 {
+	return Int32(i.Raw())
+}
+
+func (i *Int) ToInt16() Int16 {
+	return Int16(i.Raw())
+}
+
+func (i *Int) ToInt8() Int8 {
+	return Int8(i.Raw())
+}
+
+func (i *Int) ToFloat64() Float64 {
+	return Float64(i.Raw())
+}
+
+func (i *Int) ToFloat32() Float32 {
+	return Float32(i.Raw())
 }
